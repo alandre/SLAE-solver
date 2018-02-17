b82d5db1bf95d4d0bd998e7e2791005e93a39969
Evgeny Korzh <shekaso@mail.ru>
2018-02-17 13:40:44 +0700

    Добавлены интерфейсы IMethod, ISolver, ILogger. Реализован класс Solver

---
+++ UI/SolverCore/Interfaces/ILogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolverCore
+{
+    public interface ILogger
+    {
+        //Наброски
+        void write();
+        void read();
+        
+    }
+}
---
+++ UI/SolverCore/Interfaces/ISolver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolverCore
+{
+    public interface ISolver
+    {
+        /// <summary>
+        /// Решение СЛАУ Ax=b
+        /// </summary>
+        /// <param name="A">Матрица СЛАУ</param>
+        /// <param name="x0">Начальное приблежение</param>
+        /// <param name="b">Вектор правой части СЛАУ</param>
+        /// <param name="max_iter">Макимальное число иттераций</param>
+        /// <param name="eps">Значение относительной невязки для выхода</param>
+        /// <param name="malloc">false - результат сохраняется в вектор x0, true - выделяется новый вектор</param>
+        /// <returns>Вектор с результатом</returns>
+        IVector Solve(IMatrix A, IVector x0, IVector b, int maxiter, double eps, bool malloc = false);
+    }
+}

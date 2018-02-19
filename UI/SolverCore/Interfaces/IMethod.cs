d25c5880a903046205c248bbefa6d092541eec85
Evgeny Korzh <shekaso@mail.ru>
2018-02-19 22:07:49 +0700

    Поправлен метод якоби, и имена переменных

--- UI/SolverCore/Interfaces/IMethod.cs
+++ UI/SolverCore/Interfaces/IMethod.cs
@@ -14,11 +14,11 @@ namespace SolverCore
         /// <param name="A">Матрица СЛАУ</param>
         /// <param name="x0">Начальное приблежение</param>
         /// <param name="b">Вектор правой части СЛАУ</param>
-        /// <param name="max_iter">Макимальное число иттераций</param>
+        /// <param name="maxIter">Макимальное число итераций</param>
         /// <param name="eps">Значение относительной невязки для выхода</param>
         /// <param name="malloc">false - результат сохраняется в вектор x0, true - выделяется новый вектор</param>
         /// <returns>Вектор с результатом</returns>
-        IVector InitMethod(ILinearOperator A, IVector x0, IVector b, int max_iter, double eps, bool malloc = false);
+        IVector InitMethod(ILinearOperator A, IVector x0, IVector b, int maxIter, double eps, bool malloc = false);
 
         /// <summary>
         /// Делает шаг иттерационного метода
--- UI/SolverCore/Interfaces/ISolver.cs
+++ UI/SolverCore/Interfaces/ISolver.cs
@@ -14,10 +14,10 @@ namespace SolverCore
         /// <param name="A">Матрица СЛАУ</param>
         /// <param name="x0">Начальное приблежение</param>
         /// <param name="b">Вектор правой части СЛАУ</param>
-        /// <param name="max_iter">Макимальное число иттераций</param>
+        /// <param name="maxIter">Макимальное число итераций</param>
         /// <param name="eps">Значение относительной невязки для выхода</param>
         /// <param name="malloc">false - результат сохраняется в вектор x0, true - выделяется новый вектор</param>
         /// <returns>Вектор с результатом</returns>
-        IVector Solve(ILinearOperator A, IVector x0, IVector b, int maxiter, double eps, bool malloc = false);
+        IVector Solve(ILinearOperator A, IVector x0, IVector b, int maxIter, double eps, bool malloc = false);
     }
 }

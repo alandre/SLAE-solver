11891d72dc434bc1e8942cb62b8b43ad4ba5dd22
Katya <carpediem.mask@gmail.com>
2018-03-05 01:17:08 +0700

    добавлен профильный формат матрицы без перевода в координатный формат

--- UI/SolverCore/MatrixFormats/Skyline/SkylineMatrix.cs
+++ UI/SolverCore/MatrixFormats/Skyline/SkylineMatrix.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace SolverCore
 {
@@ -8,82 +9,458 @@ namespace SolverCore
     /// </summary>
     class SkylineMatrix : IMatrix, ILinearOperator, ITransposeLinearOperator
     {
-        public double this[int i, int j] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        private double[] di; // диагональ
+        private double[] al; // массив элементов профиля нижнего треугольника
+        private double[] au; // массив элементов профиля верхнего треугольника
+        private int[] ia; // целочисленный массив с указателями начала строк профиля [0,0,...]
 
-        public int Size => throw new NotImplementedException();
+        //конструктор
+        public SkylineMatrix(double[] di, int[] ia, double[] al, double[] au)
+        {
+            if (di == null)
+            {
+                throw new ArgumentNullException(nameof(di));
+            }
 
-        public IVector Diagonal => throw new NotImplementedException();
+            if (ia == null)
+            {
+                throw new ArgumentNullException(nameof(ia));
+            }
 
-        public ILinearOperator Transpose => throw new NotImplementedException();
+            if (al == null)
+            {
+                throw new ArgumentNullException(nameof(al));
+            }
+
+            if (au == null)
+            {
+                throw new ArgumentNullException(nameof(au));
+            }
+
+            var size = di.Length;
+            this.di = new double[size];
+            di.CopyTo(this.di, 0);
+
+            var size1 = ia.Length;
+            if (size1 != size + 1)
+            {
+                throw new ArgumentNullException("the massive ia is not properly filled", nameof(ia));
+            }
+            this.ia = new int[size1];
+            ia.CopyTo(this.ia, 0);
+            
+            var size2 = al.Length;
+            var size3 = au.Length;
+            if(this.ia[size1 - 1] != size2)
+            {
+                throw new ArgumentNullException("the massive ia or al is not properly filled", nameof(al));
+            }
+            if (this.ia[size1 - 1] != size2 || this.ia[size1 - 1] != size3)
+            {
+                throw new ArgumentNullException("the massive ia or al or au is not properly filled", nameof(al));
+            }
+            this.al = new double[size2];
+            al.CopyTo(this.al, 0);  
+            
+            this.au = new double[size2];
+            au.CopyTo(this.au, 0); 
+        }
+
+        public double this[int i, int j]
+        {
+            get
+            {
+                try
+                {
+                    if (i == j) return di[i]; // если диагональ
+
+                    if (i > j)
+                    {
+                        if (ia[i + 1] - ia[i] == 0) return 0; // если данный элемент не содержится в профиле, значит, он нулевой
+
+                        int k = i - (ia[i + 1] - ia[i]); // индекс столбца первого элемента в профиле
+                        return al[ia[i] + j - k];
+                    }
+                    else
+                    {
+                        if (ia[i + 1] - ia[i] == 0) return 0; // если данный элемент не содержится в профиле, значит, он нулевой
+
+                        int k = i - (ia[i + 1] - ia[i]); // индекс строки первого элемента в профиле
+                        return au[ia[i] + j - k];
+                    }
+                }
+                catch(IndexOutOfRangeException)
+                {
+                    throw new IndexOutOfRangeException(); //если выходим за рамки размерности матрицы
+                }
+            }
+        }
+
+        public SkylineMatrix(int size, int size2)
+        {
+            di = new double[size];
+            ia = new int[size + 1];
+            al = new double[size2];
+            au = new double[size2];
+        }
+
+        public int Size => di.Length;
+
+        //диагональ
+        public IVector Diagonal
+        {
+            get
+            {
+                var diagonal = new Vector(Size);
+
+                for(int i = 0; i < Size; i++)
+                {
+                    diagonal[i] = di[i];
+                }
+
+                return diagonal;
+            }
+        }
+
+        public ILinearOperator Transpose => new TransposeMatrix<SkylineMatrix> { Matrix = this };
+
+        public IEnumerator<(double value, int row, int col)> GetEnumerator()
+        {
+            for (int i = 1; i < Size; i++)
+            {
+                int ia1 = ia[i];
+                int ia2 = ia[i + 1];
+                int k = i - (ia2 - ia1);
+                for ( ; ia1 < ia2; ia1++, k++)
+                {
+                    yield return (al[ia1], i, k);
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        // заполнение матрицы
+        public void Fill(FillFunc elems) 
+        { 
+            if (elems == null) 
+            { 
+                throw new ArgumentNullException(nameof(elems)); 
+            } 
+            int i = 0, ju = 0, jl=0, k = 0; 
+            ia[0] = 0; 
+            ia[1] = 0; 
+            foreach (var elem in this) 
+            {
+                if (elem.col == elem.row)
+                {
+                    di[i] = elems(elem.row, elem.col);
+                }
+                else
+                if (elem.col > elem.row)
+                {
+                    au[ju] = elems(elem.row, elem.col);
+                    ju++;
+                }
+                else
+                if (elem.col < elem.row)
+                {
+                    al[jl] = elems(elem.row, elem.col);
+                    jl++;
+                }
+
+                if (elem.row == i && i > 0)
+                {
+                    k++; // подсчет количества элементов в профиле
+                }    
+                else
+                {
+                    ia[i+1] = k;
+                    i++;
+                    k = 0;
+                }
+            } 
+        }
+
+        //умножение на нижний треугольник
+        public IVector LMult(IVector vector, bool isUseDiagonal, DiagonalElement diagonalElement = DiagonalElement.One)
+        {
+            if (vector == null)
+            {
+                throw new ArgumentNullException(nameof(vector));
+            }
+
+            if (vector.Size != Size)
+            {
+                throw new RankException();
+            }
+
+            var result = new Vector(Size);
+
+            for (int i = 0; i < Size; i++)
+            {
+                int k = ia[i];
+                for (int j = i - (ia[i + 1] - k); j < i; j++, k++)
+                    result[i] += al[k] * vector[j];
+            }
+            
+            for (int i = 0; i < Size; i++)
+                result[i] += isUseDiagonal ? di[i] * vector[i] : (double)diagonalElement * vector[i];
+
+            return result;
+        }
+
+        //умножение на нижний треугольник трансп
+        public IVector LMultTranspose(IVector vector, bool isUseDiagonal, DiagonalElement diagonalElement = DiagonalElement.One)
+        {
+            if (vector == null)
+            {
+                throw new ArgumentNullException(nameof(vector));
+            }
+
+            if (vector.Size != Size)
+            {
+                throw new RankException();
+            }
+
+            var result = new Vector(Size);
+
+            for (int i = 0; i < Size; i++)
+            {
+                int k = ia[i];
+                for (int j = i - (ia[i + 1] - k); j < i; j++, k++)
+                    result[j] += al[k] * vector[i];
+            }
+
+           for (int i = 0; i < Size; i++)
+                result[i] += isUseDiagonal ? di[i] * vector[i] : (double)diagonalElement * vector[i];
+
+            return result;
+        }
+
+        //умножение матрицы на вектор
+        public IVector Multiply(IVector vector)
+        {
+            if (vector == null)
+            {
+                throw new ArgumentNullException(nameof(vector));
+            }
+
+            if (vector.Size != Size)
+            {
+                throw new RankException();
+            }
+
+            var result = new Vector(Size);
+
+            for (int i = 0; i < Size; i++)
+            {
+                int k = ia[i];
+                for (int j = i - (ia[i + 1] - k); j < i; j++, k++)
+                {
+                    vector[i] += al[k] * vector[j];
+                    vector[j] += au[k] * vector[i];
+                }
+                vector[i] += di[i] * vector[i];
+            }
+            return result;
+        }
+
+        //умножение матрицы на вектор трансп
+        public IVector MultiplyTranspose(IVector vector)
+        {
+            if (vector == null)
+            {
+                throw new ArgumentNullException(nameof(vector));
+            }
+
+            if (vector.Size != Size)
+            {
+                throw new RankException();
+            }
+
+            var result = new Vector(Size);
+
+            for (int i = 0; i < Size; i++)
+            {
+                int k = ia[i];
+                for (int j = i - (ia[i + 1] - k); j < i; j++, k++)
+                {
+                    vector[i] += au[k] * vector[j];
+                    vector[j] += al[k] * vector[i];
+                }
+                vector[i] += di[i] * vector[i];
+            }
+            return result;
+        }
+
+
+        //умножение на верхний треугольник
+        public IVector UMult(IVector vector, bool isUseDiagonal, DiagonalElement diagonalElement = DiagonalElement.One)
+        {
+            if (vector == null)
+            {
+                throw new ArgumentNullException(nameof(vector));
+            }
+
+            if (vector.Size != Size)
+            {
+                throw new RankException();
+            }
+
+            var result = new Vector(Size);
+
+            for (int j = 0; j < Size; j++)
+            {
+                int k = ia[j];
+                for (int i = j - (ia[j + 1] - k); i < j; i++, k++)
+                    result[i] += au[k] * vector[j];
+            }
+
+            for (int i = 0; i < Size; i++)
+                result[i] += isUseDiagonal ? di[i] * vector[i] : (double)diagonalElement * vector[i];
+
+            return result;
+        }
+        
+        //умножение на верхний треугольник трансп
+        public IVector UMultTranspose(IVector vector, bool isUseDiagonal, DiagonalElement diagonalElement = DiagonalElement.One)
+        {
+            if (vector == null)
+            {
+                throw new ArgumentNullException(nameof(vector));
+            }
+
+            if (vector.Size != Size)
+            {
+                throw new RankException();
+            }
+
+            var result = new Vector(Size);
+
+            for (int j = 0; j < Size; j++)
+            {
+                int k = ia[j];
+                for (int i = j - (ia[j + 1] - k); i < j; i++, k++)
+                    result[j] += au[k] * vector[i];
+            }
+
+            for (int i = 0; i < Size; i++)
+                result[i] += isUseDiagonal ? di[i] * vector[i] : (double)diagonalElement * vector[i];
+
+            return result;
+        }
+
+        //прямой ход
+        public IVector LSolve(IVector vector, bool isUseDiagonal)
+        {
+            if (vector == null)
+            {
+                throw new ArgumentNullException(nameof(vector));
+            }
+
+            if (vector.Size != Size)
+            {
+                throw new RankException();
+            }
+
+            var result = vector.Clone();
+
+            for (int i = 0; i < Size; i++)
+            {
+                double sum = 0;
+                int k = i - (ia[i + 1] - ia[i]);
+                for (int j = ia[i]; k < i; j++, k++)
+                    sum += al[j] * result[k];
+                result[i] = isUseDiagonal ? (vector[i] - sum) / di[i] : vector[i] - sum;
+            }
+            
+            return result;
+        }
+
+        //прямой ход трансп
+        public IVector LSolveTranspose(IVector vector, bool isUseDiagonal)
+        {
+            if (vector == null)
+            {
+                throw new ArgumentNullException(nameof(vector));
+            }
+
+            if (vector.Size != Size)
+            {
+                throw new RankException();
+            }
+
+            var result = vector.Clone();
+
+            for (int i = Size - 1; i >= 0; i--)
+            {
+                if (isUseDiagonal) result[i] /= di[i];
+                int k = i - (ia[i + 1] - ia[i]);
+                for (int j = ia[i + 1] - 1, m = i - 1; m >= k; j--, m--)
+                    result[m] -= al[j] * result[i];
+            }
+            
+            return result;
+        }
+
+        //обратный ход
+        public IVector USolve(IVector vector, bool isUseDiagonal)
+        {
+            if (vector == null)
+            {
+                throw new ArgumentNullException(nameof(vector));
+            }
+
+            if (vector.Size != Size)
+            {
+                throw new RankException();
+            }
+
+            var result = vector.Clone();
+            
+            for (int i = Size - 1; i >= 0; i--)
+            {
+                if (isUseDiagonal) result[i] /= di[i];
+                int k = i - (ia[i + 1] - ia[i]);
+                for (int j = ia[i + 1] - 1, m = i - 1; m >= k; j--, m--)
+                    result[m] -= au[j] * result[i];
+            }
+            
+            return result;
+        }
+
+        //обратный ход трансп
+        public IVector USolveTranspose(IVector vector, bool isUseDiagonal)
+        {
+            if (vector == null)
+            {
+                throw new ArgumentNullException(nameof(vector));
+            }
+
+            if (vector.Size != Size)
+            {
+                throw new RankException();
+            }
+
+            var result = vector.Clone();
+            
+            for (int i = 0; i < Size; i++)
+            {
+                double sum = 0;
+                int k = i - (ia[i + 1] - ia[i]);
+                for (int j = ia[i]; k < i; j++, k++)
+                    sum += au[j] * result[k];
+                result[i] = isUseDiagonal ? (vector[i] - sum) / di[i] : vector[i] - sum;
+            }
+            
+            return result;
+        }
 
         public CoordinationalMatrix ConvertToCoordinationalMatrix()
         {
+            // перевод в координатную
             throw new NotImplementedException();
         }
-
-        public void Fill(FillFunc elems)
-        {
-            throw new NotImplementedException();
-        }
-
-        public System.Collections.Generic.IEnumerator<(double value, int row, int col)> GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
-
-        public IVector LMult(IVector x, bool UseDiagonal, DiagonalElement diagonalElement = DiagonalElement.One)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IVector LMultTranspose(IVector vector, bool isUseDiagonal, DiagonalElement diagonalElement = DiagonalElement.One)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IVector LSolve(IVector x, bool UseDiagonal)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IVector LSolveTranspose(IVector vector, bool isUseDiagonal)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IVector Multiply(IVector x)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IVector MultiplyTranspose(IVector vector)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IVector UMult(IVector x, bool UseDiagonal, DiagonalElement diagonalElement = DiagonalElement.One)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IVector UMultTranspose(IVector vector, bool isUseDiagonal, DiagonalElement diagonalElement = DiagonalElement.One)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IVector USolve(IVector x, bool UseDiagonal)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IVector USolveTranspose(IVector vector, bool isUseDiagonal)
-        {
-            throw new NotImplementedException();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
-    }
+    }        
 }
--- UI/SolverCore/MatrixFormats/Skyline/SymmetricSkylineMatrix.cs
+++ UI/SolverCore/MatrixFormats/Skyline/SymmetricSkylineMatrix.cs
@@ -6,57 +6,298 @@ namespace SolverCore
 {
     public class SymmetricSkylineMatrix : IMatrix, ILinearOperator
     {
-        public double this[int i, int j] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        private double[] di; // диагональ
+        private double[] al; // массив элементов профиля
+        private int[] ia; // целочисленный массив с указателями начала строк профиля [0,0,...]
 
-        public int Size => throw new NotImplementedException();
-
-        public IVector Diagonal => throw new NotImplementedException();
-
-        public ILinearOperator Transpose => throw new NotImplementedException();
-
-        public CoordinationalMatrix ConvertToCoordinationalMatrix()
+        //конструктор
+        public SymmetricSkylineMatrix(double[] di, int[] ia, double[] al)
         {
-            throw new NotImplementedException();
+            if (di == null)
+            {
+                throw new ArgumentNullException(nameof(di));
+            }
+
+            if (ia == null)
+            {
+                throw new ArgumentNullException(nameof(ia));
+            }
+
+            if (al == null)
+            {
+                throw new ArgumentNullException(nameof(al));
+            }
+
+            var size = di.Length;
+            this.di = new double[size];
+            di.CopyTo(this.di, 0);
+
+            var size1 = ia.Length;
+            if (size1 != size + 1)
+            {
+                throw new ArgumentNullException("the massive ia is not properly filled", nameof(ia));
+            }
+            this.ia = new int[size1];
+            ia.CopyTo(this.ia, 0);
+
+            var size2 = al.Length;
+            if(this.ia[size1 - 1] != size2)
+            {
+                throw new ArgumentNullException("the massive ia or al is not properly filled", nameof(al));
+            }
+            this.al = new double[size2];
+            al.CopyTo(this.al, 0);    
         }
 
-        public void Fill(FillFunc elems)
+        public SymmetricSkylineMatrix(int size, int size2)
         {
-            throw new NotImplementedException();
+            di = new double[size];
+            ia = new int[size + 1];
+            al = new double[size2];
         }
+
+        //Получение значения по индексу
+        public double this[int i, int j]
+        {
+            get
+            {
+                try
+                {
+                    if (i == j) return di[i]; // если диагональ
+
+                    if (j > i)
+                    {
+                        var a = i;
+                        i = j;
+                        j = a;
+                    }
+
+                    if (ia[i + 1] - ia[i] == 0) return 0; // если данный элемент не содержится в профиле, значит, он нулевой
+
+                    int k = i - (ia[i + 1] - ia[i]); // индекс столбца первого элемента в профиле
+                    return al[ia[i] + j - k];
+                }
+                catch(IndexOutOfRangeException)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        public int Size => di.Length;
+
+        //диагональ
+        public IVector Diagonal
+        {
+            get
+            {
+                var diagonal = new Vector(Size);
+
+                for(int i = 0; i < Size; i++)
+                {
+                    diagonal[i] = di[i];
+                }
+
+                return diagonal;
+            }
+        }
+        
+        public ILinearOperator Transpose => this;
 
         public IEnumerator<(double value, int row, int col)> GetEnumerator()
         {
+            for (int i = 1; i < Size; i++)
+            {
+                int ia1 = ia[i];
+                int ia2 = ia[i + 1];
+                int k = i - (ia2 - ia1);
+                for ( ; ia1 < ia2; ia1++, k++)
+                {
+                    yield return (al[ia1], i, k);
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        // заполнение матрицы
+        public void Fill(FillFunc elems) 
+        { 
+            if (elems == null) 
+            { 
+                throw new ArgumentNullException(nameof(elems)); 
+            } 
+            int i = 0, j = 0, k = 0; 
+            ia[0] = 0; 
+            ia[1] = 0; 
+            foreach (var elem in this) 
+            {
+                if (elem.col == elem.row)
+                {
+                    di[i] = elems(elem.row, elem.col);
+                }
+                else
+                if (elem.col < elem.row)
+                {
+                    al[j] = elems(elem.row, elem.col);
+                    j++;
+                }
+
+                if (elem.row == i && i > 0)
+                {
+                    k++; // подсчет количества элементов в профиле
+                }    
+                else
+                {
+                    ia[i+1] = k;
+                    i++;
+                    k = 0;
+                }
+            } 
+        }
+
+        public CoordinationalMatrix ConvertToCoordinationalMatrix()
+        {
+            // перевод в координатную
             throw new NotImplementedException();
         }
 
-        public IVector LMult(IVector x, bool UseDiagonal, DiagonalElement diagonalElement = DiagonalElement.One)
+        //умножение на нижний треугольник
+        public IVector LMult(IVector vector, bool isUseDiagonal, DiagonalElement diagonalElement = DiagonalElement.One)
         {
-            throw new NotImplementedException();
+            if (vector == null)
+            {
+                throw new ArgumentNullException(nameof(vector));
+            }
+
+            if (vector.Size != Size)
+            {
+                throw new RankException();
+            }
+
+            var result = new Vector(Size);
+
+            for (int i = 0; i < Size; i++)
+            {
+                int k = ia[i];
+                for (int j = i - (ia[i + 1] - k); j < i; j++, k++)
+                    result[i] += al[k] * vector[j];
+            }
+
+            for (int i = 0; i < Size; i++)
+                result[i] += isUseDiagonal ? di[i] * vector[i] : (double)diagonalElement * vector[i];
+
+            return result;
         }
 
-        public IVector LSolve(IVector x, bool UseDiagonal)
+        //умножение на верхний треугольник
+        public IVector UMult(IVector vector, bool isUseDiagonal, DiagonalElement diagonalElement = DiagonalElement.One)
         {
-            throw new NotImplementedException();
+            if (vector == null)
+            {
+                throw new ArgumentNullException(nameof(vector));
+            }
+
+            if (vector.Size != Size)
+            {
+                throw new RankException();
+            }
+
+            var result = new Vector(Size);
+
+            for (int j = 0; j < Size; j++)
+            {
+                int k = ia[j];
+                for (int i = j - (ia[j + 1] - k); i < j; i++, k++)
+                    result[i] += al[k] * vector[j];
+            }
+
+            for (int i = 0; i < Size; i++)
+                result[i] += isUseDiagonal ? di[i] * vector[i] : (double)diagonalElement * vector[i];
+
+            return result;
         }
 
-        public IVector Multiply(IVector x)
+        //умножение матрицы на вектор
+        public IVector Multiply(IVector vector)
         {
-            throw new NotImplementedException();
+            if (vector == null)
+            {
+                throw new ArgumentNullException(nameof(vector));
+            }
+
+            if (vector.Size != Size)
+            {
+                throw new RankException();
+            }
+            
+            var result = new Vector(Size);
+
+            for (int i = 0; i < Size; i++)
+            {
+                int k = ia[i];
+                for (int j = i - (ia[i + 1] - k); j < i; j++, k++)
+                {
+                    vector[i] += al[k] * vector[j];
+                    vector[j] += al[k] * vector[i];
+                }
+                vector[i] += di[i] * vector[i];
+            }
+            return result;
         }
 
-        public IVector UMult(IVector x, bool UseDiagonal, DiagonalElement diagonalElement = DiagonalElement.One)
+        //прямой ход
+        public IVector LSolve(IVector vector, bool isUseDiagonal)
         {
-            throw new NotImplementedException();
+            if(vector == null)
+            {
+                throw new ArgumentNullException(nameof(vector));
+            }
+
+            if (vector.Size != Size)
+            {
+                throw new RankException();
+            }
+
+            var result = vector.Clone();
+
+            for (int i = 0; i < Size; i++)
+            {
+                double sum = 0;
+                int k = i - (ia[i + 1] - ia[i]);
+                for (int j = ia[i]; k < i; j++, k++)
+                    sum += al[j] * result[k];
+                result[i] = isUseDiagonal ? (vector[i] - sum) / di[i] : vector[i] - sum;
+            }
+
+            return result;
         }
 
-        public IVector USolve(IVector x, bool UseDiagonal)
+        //обратный ход
+        public IVector USolve(IVector vector, bool isUseDiagonal)
         {
-            throw new NotImplementedException();
+            if (vector == null)
+            {
+                throw new ArgumentNullException(nameof(vector));
+            }
+
+            if (vector.Size != Size)
+            {
+                throw new RankException();
+            }
+
+            var result = vector.Clone();
+            
+            for (int i = Size - 1; i >= 0; i--)
+            {
+                if (isUseDiagonal) result[i] = result[i] / di[i];
+                int k = i - (ia[i + 1] - ia[i]);
+                for (int j = ia[i + 1] - 1, m = i - 1; m >= k; j--, m--)
+                    result[m] -= al[j] * result[i];
+            }
+            
+            return result;
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
     }
 }

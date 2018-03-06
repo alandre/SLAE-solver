b50cc02c66d792c2508b2b303e99b2573fabf001
AndrewUsoltsev <usol1996@gmail.com>
2018-03-06 20:36:31 +0700

    В связи с косяком ветке при rebase возникла необходимость полностью обновить проект Tests

---
+++ UI/Tests/MF/SparseRow/TestSymmetricSparseRowMatrix.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+using SolverCore;
+
+namespace MF.SymmetricSparseRow
+{
+    public class TestSymmetricSparseRowMatrix
+    {
+        private double[] _a;
+        private int[] _ia;
+        private int[] _ja;
+        private IVector vector;
+
+        private SymmetricSparseRowMatrix sparseRowMatrix;
+
+
+        public TestSymmetricSparseRowMatrix()
+        {
+            _a = new double[] { 1, 2, 5, 3, 7, 4 };
+            _ia = new int[] { 0, 1, 2, 4, 6 };
+            _ja = new int[] { 0, 1, 0, 2, 1, 3 };
+
+            vector = new Vector(new double[] { 2, 1, 1, 1 });
+
+            sparseRowMatrix = new SymmetricSparseRowMatrix(_a, _ja, _ia);
+        }
+
+        [Fact]
+        public void SparseRowMatrix_TestLMult()
+        {
+            var resultTrueDiag = sparseRowMatrix.LMult(vector, true);
+            Vector resultActualTrueDiag = new Vector(new double[] { 2, 2, 13, 11 });
+
+            var resultFalseDiag = sparseRowMatrix.LMult(vector, false);
+            Vector resultActualFalseDiag = new Vector(new double[] { 2, 1, 11, 8 });
+
+            for (int i = 0; i < resultTrueDiag.Size; i++)
+            {
+                Assert.Equal(resultTrueDiag[i], resultActualTrueDiag[i], 8);
+                Assert.Equal(resultFalseDiag[i], resultActualFalseDiag[i], 8);
+            }
+        }
+
+        [Fact]
+        public void SparseRowMatrix_TestUMult()
+        {
+            var resultTrueDiag = sparseRowMatrix.UMult(vector, true);
+            Vector resultActualTrueDiag = new Vector(new double[] { 7, 9, 3, 4 });
+
+            var resultFalseDiag = sparseRowMatrix.UMult(vector, false);
+            Vector resultActualFalseDiag = new Vector(new double[] { 7, 8, 1, 1 });
+
+            for (int i = 0; i < resultTrueDiag.Size; i++)
+            {
+                Assert.Equal(resultTrueDiag[i], resultActualTrueDiag[i], 8);
+                Assert.Equal(resultFalseDiag[i], resultActualFalseDiag[i], 8);
+            }
+        }
+
+        [Fact]
+        public void SparseRowMatrix_TestLSolve()
+        {
+            IVector resultActual = new Vector(new double[] { 1, 2, 3, 4 });
+            IVector vector = sparseRowMatrix.LMult(resultActual, true);
+
+            var result = sparseRowMatrix.LSolve(vector, true);
+
+            for (int i = 0; i < result.Size; i++)
+                Assert.Equal(result[i], resultActual[i], 8);
+        }
+
+        [Fact]
+        public void SparseRowMatrix_TestUSolve()
+        {
+            IVector resultActual = new Vector(new double[] { 1, 2, 3, 4 });
+            IVector vector = sparseRowMatrix.UMult(resultActual, true);
+
+            var result = sparseRowMatrix.USolve(vector, true);
+
+            for (int i = 0; i < result.Size; i++)
+                Assert.Equal(result[i], resultActual[i], 8);
+        }
+
+        [Fact]
+        public void SymmitricSparseRowMatrix_TesMultiply()
+        {
+            var result = sparseRowMatrix.Multiply(vector);
+            Vector resultActual = new Vector(new double[] { 7, 9, 13, 11 });
+
+            for (int i = 0; i < resultActual.Size; i++)
+                Assert.Equal(result[i], resultActual[i], 8);
+        }
+    }
+}
--- UI/Tests/MF/TestsDenseMatrix.cs
+++ UI/Tests/MF/TestsDenseMatrix.cs
@@ -1,25 +1,179 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
+using Xunit;
+using SolverCore;
 
-namespace Tests
+namespace MF.DenseMatrixTests
 {
-    [TestClass]
     public class TestsDenseMatrix
     {
-        [TestMethod]
+        private double[,] _matrix;
+
+        [Fact]
+        public void DenseMatrix_TestConstructorExeptions()
+        {
+            _matrix = new double[2, 3] { { 1, 3, 5 }, { 2, 5, 4 } };
+
+            Assert.Throws<ArgumentNullException>(() => { DenseMatrix denseMatrix = new DenseMatrix(null); });
+            Assert.Throws<ArgumentException>(() => { DenseMatrix denseMatrix = new DenseMatrix(_matrix); });
+            Assert.Throws<ArgumentException>(() => { DenseMatrix denseMatrix2 = new DenseMatrix(-1); }); 
+        }
+
+        [Fact]
+        public void DenseMatrix_TestIndexator()
+        {
+            _matrix = new double[3, 3] { { 1, 3, 5 }, { 2, 5, 4 }, { 7, 1, 8 } };
+            DenseMatrix denseMatrix = new DenseMatrix(_matrix);
+
+            Assert.Throws<IndexOutOfRangeException>(() =>  denseMatrix[4,2]);
+            Assert.Equal(1.0,denseMatrix[2,1]);
+        }
+
+        [Fact]
         public void DenseMatrix_Foreach()
         {
-            var di = new double[] { 1, 1, 1, 1 };
-            var al = new double[] { 1, 2, 3 };
-            var au = new double[] { 4, 5, 6 };
-            var ia = new int[] { 1, 1, 1, 2, 4 };
-            var ja = new int[] { 1, 1, 2 };
-            var matrix = new SolverCore.SparseRowColumnMatrix(di, al, au, ia, ja);
+            _matrix = new double[3, 3] { { 1, 3, 5 }, { 2, 5, 4 }, { 7, 1, 8 } };
+            var denseMatrix = new DenseMatrix(_matrix);
 
-            foreach(var elem in matrix)
+            var res = denseMatrix.Multiply(new Vector(new double[] { 1, 1, 1 }));
+            Vector resVector = new Vector(new double[] { 9, 11, 16 });
+
+            int i = 0;
+            foreach (var el in res)
             {
-                Console.WriteLine($"({elem.row}, {elem.col}): {elem.value}");
+                Assert.Equal(el, resVector[i]);
+                i++;
             }
         }
+
+        [Fact]
+        public void DenseMatrix_TestLMultExeptions()
+        {
+            _matrix = new double[3, 3] { { 1, 3, 5 }, { 2, 5, 4 }, { 7, 1, 8 } };
+            Vector exampleVector = new Vector(new double[2] { 1.0, 2.0 });
+            DenseMatrix denseMatrix = new DenseMatrix(_matrix);
+
+            Assert.Throws<ArgumentNullException>(() => denseMatrix.LMult(null,false, DiagonalElement.One));
+            Assert.Throws<RankException>(() => denseMatrix.LMult(exampleVector, false, DiagonalElement.One));
+        }
+     
+        [Fact]
+        public void DenseMatrix_TestLMult()
+        {
+            _matrix = new double[3, 3] { { 1, 3, 5 }, { 2, 5, 4 }, { 7, 1, 8 } };
+            DenseMatrix denseMatrix = new DenseMatrix(_matrix);
+
+            Vector vector = new Vector(new double[] { 1, 3, 8 });
+
+            var resultTrueDiag = denseMatrix.LMult(vector, true);
+            Vector resultActualTrueDiag = new Vector(new double[] { 1, 17, 74 });
+
+            var resultFalseDiag = denseMatrix.LMult(vector, false);
+            Vector resultActualFalseDiag = new Vector(new double[] { 1, 5, 18 });
+
+            for (int i = 0; i < resultTrueDiag.Size; i++)
+            {
+                Assert.Equal(resultTrueDiag[i], resultActualTrueDiag[i], 8);
+                Assert.Equal(resultFalseDiag[i], resultActualFalseDiag[i], 8);
+            }
+        }
+
+        [Fact]
+        public void DenseMatrix_TestUMultExeptions()
+        {
+            _matrix = new double[3, 3] { { 1, 3, 5 }, { 2, 5, 4 }, { 7, 1, 8 } };
+            Vector exampleVector = new Vector(new double[2] { 1.0, 2.0 });
+            DenseMatrix denseMatrix = new DenseMatrix(_matrix);
+
+            Assert.Throws<ArgumentNullException>(() => denseMatrix.UMult(null, false, DiagonalElement.One));
+            Assert.Throws<RankException>(() => denseMatrix.UMult(exampleVector, false, DiagonalElement.One));
+        }
+
+        [Fact]
+        public void DenseMatrix_TestUMult()
+        {
+            _matrix = new double[3, 3] { { 1, 3, 5 }, { 2, 5, 4 }, { 7, 1, 8 } };
+            DenseMatrix denseMatrix = new DenseMatrix(_matrix);
+
+            Vector vector = new Vector(new double[] { 1, 3, 8 });
+
+            var resultFalseDiag = denseMatrix.UMult(vector, false);
+            Vector resultActualFalseDiag = new Vector(new double[] { 50, 35, 8 });
+
+            var resultTrueDiag = denseMatrix.UMult(vector, true);
+            Vector resultActualTrueDiag = new Vector(new double[] { 50, 47, 64 });
+
+            for (int i = 0; i < resultFalseDiag.Size; i++)
+            {
+                Assert.Equal(resultFalseDiag[i], resultActualFalseDiag[i], 8);
+                Assert.Equal(resultTrueDiag[i], resultActualTrueDiag[i], 8);
+            }
+        }
+
+        [Fact]
+        public void DenseMatrix_TestLSolveExeptions()
+        {
+            _matrix = new double[3, 3] { { 1, 3, 5 }, { 2, 5, 4 }, { 7, 1, 8 } };
+            Vector exampleVector = new Vector(new double[2] { 1.0, 2.0 });
+            DenseMatrix denseMatrix = new DenseMatrix(_matrix);
+
+            Assert.Throws<ArgumentNullException>(() => denseMatrix.LSolve(null, false));
+            Assert.Throws<RankException>(() => denseMatrix.LSolve(exampleVector, false));
+        }
+
+        [Fact]
+        public void DenseMatrix_TestLSolve()
+        {
+            _matrix = new double[3, 3] { { 1, 2, 3 }, { 2, -1, 1 }, { 7, -20, 93 } };
+            DenseMatrix denseMatrix = new DenseMatrix(_matrix);
+
+            Vector vector = new Vector(new double[] { 1, 2, 3 });
+
+            var result = denseMatrix.LSolve(vector, true);
+            Vector resultActual = new Vector(new double[] { 1, 0, -0.04301075 });
+
+            for (int i = 0; i < result.Size; i++)
+                Assert.Equal(result[i], resultActual[i], 8);
+        }
+
+        [Fact]
+        public void DenseMatrix_TestUSolveExeptions()
+        {
+            _matrix = new double[3, 3] { { 1, 3, 5 }, { 2, 5, 4 }, { 7, 1, 8 } };
+            Vector exampleVector = new Vector(new double[2] { 1.0, 2.0 });
+            DenseMatrix denseMatrix = new DenseMatrix(_matrix);
+
+            Assert.Throws<ArgumentNullException>(() => denseMatrix.USolve(null, false));
+            Assert.Throws<RankException>(() => denseMatrix.USolve(exampleVector, false));
+        }
+
+        [Fact]
+        public void DenseMatrix_TestUSolve()
+        {
+            _matrix = new double[3, 3] { { 1, 3, 5 }, { 1, 1, 6 }, { 4, 2, 1 } };
+            DenseMatrix denseMatrix = new DenseMatrix(_matrix);
+
+            Vector vector = new Vector(new double[] { 1, 0, -0.04301075 });
+
+            var result = denseMatrix.USolve(vector, false);
+            Vector resultActual = new Vector(new double[] { 0.44086025, 0.2580645, -0.04301075 });
+            int[] precisionVector = new int[] { 2, 2, 3 };
+
+            for (int i = 0; i < result.Size; i++)
+                Assert.Equal(result[i], resultActual[i], 8);
+        }
+
+        [Fact]
+        public void DenseMatrix_TestMultyplyExceptions()
+        {
+            _matrix = new double[3, 3] { { 1, 3, 5 }, { 2, 5, 4 }, { 7, 1, 8 } };
+            DenseMatrix denseMatrix = new DenseMatrix(_matrix);
+
+            Vector vector = new Vector(new double[] { 1, 0});
+
+            Assert.Throws<ArgumentNullException>(() => denseMatrix.Multiply(null));
+            Assert.Throws<RankException>(() => denseMatrix.Multiply(vector));
+        }
+
+
     }
 }

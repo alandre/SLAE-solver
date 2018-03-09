a80369bd9cd7cd37e9175cf2a3aa70b900ed06a5
Evgeny Korzh <shekaso@mail.ru>
2018-03-09 22:02:28 +0700

    Начата реализация Фабрики Методов

---
+++ UI/SolverCore/Fabrics/SolversFabric.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolverCore
+{
+    enum MethodsEnum {CGMethod, GaussianSeidelMethod, JacobiMethod, LOSMethod, }
+    public class SolversFabric
+    {
+        ISolver Spawn(enum ILogger Logger)
+        {
+
+            return new SolverCore.Solvers.LoggingSolver()
+        }
+    }
+}

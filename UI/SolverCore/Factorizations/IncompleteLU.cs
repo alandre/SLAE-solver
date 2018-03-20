ea18292b6366c4fa21b137675b9be535770fd63e
Evgeny Korzh <shekaso@mail.ru>
2018-03-20 20:14:36 +0700

    IncompleteLU factoriczation

---
+++ UI/SolverCore/Factorizations/IncompleteLU.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolverCore.Factorizations
+{
+    public class IncompleteLU : IFactorization
+    {
+        public IVector LMult(IVector x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IVector LSolve(IVector x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IVector LTransposeMult(IVector x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IVector LTransposeSolve(IVector x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IVector UMult(IVector x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IVector USolve(IVector x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IVector UTransposeMult(IVector x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IVector UTransposeSolve(IVector x)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

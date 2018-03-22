6477988aa2dcacf53b02198620bedd0ee6ccfe46
bublej.2014 <bublej.2014@PMI-SRV-TERMINA>
2018-03-23 00:17:07 +0700

    Основа создана

--- UI/UI/ResultsForm.Designer.cs
+++ UI/UI/ResultsForm.Designer.cs
@@ -28,12 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.method = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.res_res = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.itercount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.method,
+            this.res_res,
+            this.itercount,
+            this.time});
+            this.dataGridView1.Location = new System.Drawing.Point(499, 37);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(411, 310);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // method
+            // 
+            this.method.HeaderText = "Имя метода";
+            this.method.Name = "method";
+            // 
+            // res_res
+            // 
+            this.res_res.HeaderText = "Конечная невязка";
+            this.res_res.Name = "res_res";
+            // 
+            // itercount
+            // 
+            this.itercount.HeaderText = "Количество итераций";
+            this.itercount.Name = "itercount";
+            // 
+            // time
+            // 
+            this.time.HeaderText = "Время";
+            this.time.Name = "time";
+            // 
+            // ResultsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ResultsForm";
+            this.ClientSize = new System.Drawing.Size(922, 487);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "ResultsForm";
+            this.Text = "Результаты";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn method;
+        private System.Windows.Forms.DataGridViewTextBoxColumn res_res;
+        private System.Windows.Forms.DataGridViewTextBoxColumn itercount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn time;
     }
 }
--- UI/UI/ResultsForm.cs
+++ UI/UI/ResultsForm.cs
@@ -9,6 +9,7 @@ namespace UI
 {
     public partial class ResultsForm : Form
     {
+        private Chart chart1;
         (string name, List<double> residual)[] Methods;
         int methods_number;
         public ResultsForm((string name, SolverCore.Loggers.SaveBufferLogger log)[] _Methods)
@@ -16,7 +17,6 @@ namespace UI
             methods_number = _Methods.Length;
             Methods = new(string name, List<double> residual)[methods_number];
 
-            //Methods = _Methods;//Опасность!!! передача по сслыке!
             for (int i = 0; i < methods_number; i++)
             {
                 Methods[i].name = _Methods[i].name;
@@ -26,14 +26,49 @@ namespace UI
                 for (int j = 0; j < item_num; j++)
                     Methods[i].residual[j] = _residual[j];
             }
+
+            ChartArea chartArea1 = new ChartArea();
+            Legend legend1 = new Legend();
+            this.chart1 = new Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+
+            // chart1
+            // 
+            chartArea1.Name = "Графики невязок";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(499, 285);
+            //
+            ///
+            ////
+            Series[] myGraphics = new Series[methods_number];
+            for (int i = 0; i < methods_number; i++)
+            {
+                int m = Methods[i].residual.Count;
+                myGraphics[i].Name = myGraphics[i].Legend = Methods[i].name;
+                for (int j = 0; j < m; j++)
+                    myGraphics[i].Points.AddXY(j, Methods[i].residual[j]);
+                myGraphics[i].ChartType = SeriesChartType.Line;
+
+                chart1.Series.Add(myGraphics[i]);
+            }
+            ////
+            ///
+            //
+
+            this.ClientSize = new System.Drawing.Size(933, 322);
+            this.Controls.Add(this.chart1);
+            this.Name = "ResultsForm";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+        }
             InitializeComponent();
         }
 
-        private void MakeGrapics()
-        {
-
-
-        }
         private Chart chart1;
 
         private void ResultsForm_Load(object sender, EventArgs e)
@@ -77,5 +112,6 @@ namespace UI
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
         }
+
     }
 }

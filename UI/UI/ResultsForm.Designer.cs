5dbecb4dedd4174a8f54040068eedb9bd1530a72
Elena <wind.is.my.brother@gmail.com>
2018-03-23 14:34:36 +0700

    Всё заработало, но дизайн пока ужасный

--- UI/UI/ResultsForm.Designer.cs
+++ UI/UI/ResultsForm.Designer.cs
@@ -33,53 +33,70 @@
             this.res_res = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.itercount = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
-            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.method,
             this.res_res,
             this.itercount,
             this.time});
-            this.dataGridView1.Location = new System.Drawing.Point(499, 37);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 249);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(411, 310);
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(898, 226);
             this.dataGridView1.TabIndex = 0;
             // 
             // method
             // 
             this.method.HeaderText = "Имя метода";
             this.method.Name = "method";
+            this.method.ReadOnly = true;
             // 
             // res_res
             // 
             this.res_res.HeaderText = "Конечная невязка";
             this.res_res.Name = "res_res";
+            this.res_res.ReadOnly = true;
             // 
             // itercount
             // 
             this.itercount.HeaderText = "Количество итераций";
             this.itercount.Name = "itercount";
+            this.itercount.ReadOnly = true;
             // 
             // time
             // 
             this.time.HeaderText = "Время";
             this.time.Name = "time";
+            this.time.ReadOnly = true;
+            // 
+            // chart1
+            // 
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(898, 231);
+            this.chart1.TabIndex = 0;
             // 
             // ResultsForm
             // 
-            dataGridView1.DataSource = dataSource;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(922, 487);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "ResultsForm";
             this.Text = "Результаты";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -91,5 +108,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn res_res;
         private System.Windows.Forms.DataGridViewTextBoxColumn itercount;
         private System.Windows.Forms.DataGridViewTextBoxColumn time;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
--- UI/UI/ResultsForm.cs
+++ UI/UI/ResultsForm.cs
@@ -2,20 +2,14 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Windows.Forms;
-using System.Web;
+using System.Linq;
 using System.Windows.Forms.DataVisualization.Charting;
 
 namespace UI
 {
     public partial class ResultsForm : Form
-    {// Говорили что нужно выводить время, в логерах его не нашел но в возможность выводить его есть; если нет возможности его добавить, то надо как-то вручную нули забить
-        //Если ничего не вышло разкоментить то что ниже
-        //public ResultsForm()
-        //{ }
-
-        private Chart chart1;
+    {
         (string name, List<double> residual, double time)[] Methods;
-        (string name, double residual, long maxiter, double time)[] dataSource;
         int methods_number;
 
         public ResultsForm()
@@ -25,65 +19,56 @@ namespace UI
 
         public ResultsForm((string name, SolverCore.Loggers.SaveBufferLogger log, double time)[] _Methods)
         {
+            InitializeComponent();
+
             methods_number = _Methods.Length;
             Methods = new(string name, List<double> residual, double time)[methods_number];
-            dataSource = new(string name, double residual, long maxiter, double time)[methods_number];
+            dataGridView1.Rows.Add(methods_number);
             for (int i = 0; i < methods_number; i++)
             {
-                dataSource[i].name = Methods[i].name = _Methods[i].name;//название метода
+                Methods[i].name = _Methods[i].name;//название метода
                 var _residual = _Methods[i].log.GetList();
                 
                 int item_num = _residual.Count;
-                dataSource[i].residual = _residual[item_num - 1];//конечная невязка
-                dataSource[i].maxiter = item_num;
-                dataSource[i].time = _Methods[i].time;
                 Methods[i].residual = new List<double>();
 
                 for (int j = 0; j < item_num; j++)
                     Methods[i].residual.Add(_residual[j]);
+
+                dataGridView1.Rows[i].Cells["method"].Value = _Methods[i].name;
+                dataGridView1.Rows[i].Cells["res_res"].Value = _residual[item_num - 1];
+                dataGridView1.Rows[i].Cells["itercount"].Value = item_num;
+                dataGridView1.Rows[i].Cells["time"].Value = _Methods[i].time;
             }
 
             ChartArea chartArea1 = new ChartArea();
             Legend legend1 = new Legend();
-            this.chart1 = new Chart();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
-            this.SuspendLayout();
-
-            // chart1
-            // 
             chartArea1.Name = "Графики невязок";
-            this.chart1.ChartAreas.Add(chartArea1);
+            chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(12, 12);
-            this.chart1.Name = "chart1";
-            this.chart1.Size = new System.Drawing.Size(499, 285);
-            //
-            ///
-            ////
+            chart1.Legends.Add(legend1);
+
             Series[] myGraphics = new Series[methods_number];
+
+            int maxiter = 0;
             for (int i = 0; i < methods_number; i++)
+                if (Methods[i].residual.Count > maxiter)
+                    maxiter = Methods[i].residual.Count;
+
+                for (int i = 0; i < methods_number; i++)
             {
                 myGraphics[i] = new Series();
                 int m = Methods[i].residual.Count;
                 myGraphics[i].Name = myGraphics[i].LegendText = Methods[i].name;
                 for (int j = 0; j < m; j++)
                     myGraphics[i].Points.AddXY(j, Methods[i].residual[j]);
+                for (int j = m; j < maxiter; j++)
+                    myGraphics[i].Points.AddXY(j, Methods[i].residual[m - 1]);
                 myGraphics[i].ChartType = SeriesChartType.Line;
 
                 chart1.Series.Add(myGraphics[i]);
             }
-            ////
-            ///
-            //
-
-            this.ClientSize = new System.Drawing.Size(933, 322);
-            this.Controls.Add(this.chart1);
-            this.Name = "ResultsForm";
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
-            this.ResumeLayout(false);
-            InitializeComponent();
-        }
+       }
     }
  
 }

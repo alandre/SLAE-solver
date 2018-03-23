7b01b2098da220a62e64e0c40384c162dc70f6d3
OvchinnikovaNastya <ovanase@gmail.com>
2018-03-23 13:48:09 +0700

    Исправлен метод CGM

--- UI/SolverCore/Methods/CGM.cs
+++ UI/SolverCore/Methods/CGM.cs
@@ -5,13 +5,12 @@ namespace SolverCore.Methods
     public class CGM : IMethod
     {
         IVector x0, b, z, r, Az;
-        ILinearOperator A;
+        ILinearOperator A, At;
         double norm_b, dotproduct_rr, coefficient;
         int currentIter;
         bool init;
         public IVector x { get; private set; }
 
-        //не предобусловленная система
         public bool InitMethod(ILinearOperator A, IVector x0, IVector b, bool malloc = false)
         {
             if (malloc)
@@ -22,13 +21,14 @@ namespace SolverCore.Methods
             {
                 x = x0;
             }
-
             this.x0 = x0;
             this.b = b;
             this.A = A;
+            At = A.Transpose;
             norm_b = b.Norm;
             currentIter = 0;
-            r = b.Add(A.Multiply(x0), -1);
+            //r = b.Add(A.Multiply(x0), -1);
+            r = At.Multiply(b).Add(At.Multiply(A.Multiply(x0)), -1);
             z = r.Clone();
             dotproduct_rr = r.DotProduct(r);
             init = true;
@@ -44,20 +44,25 @@ namespace SolverCore.Methods
             currentIter++;
             iter = currentIter;
 
-            Az = A.Multiply(z);
+            // Az = A.Multiply(z);
+            Az = At.Multiply(A.Multiply(z));
             coefficient = dotproduct_rr / Az.DotProduct(z);
-            x = x.Add(z, coefficient);
-            r = r.Add(Az, -coefficient);
-            coefficient = dotproduct_rr;
-            dotproduct_rr = r.DotProduct(r);
-            coefficient = dotproduct_rr / coefficient;
-            z = r.Add(z, coefficient);
-
             if (Double.IsInfinity(coefficient) || Double.IsNaN(coefficient))
             {
                 residual = -1;
                 return;
             }
+            x = x.Add(z, coefficient);
+            r = r.Add(Az, -coefficient);
+            coefficient = dotproduct_rr;
+            dotproduct_rr = r.DotProduct(r);
+            coefficient = dotproduct_rr / coefficient;
+            if (Double.IsInfinity(coefficient) || Double.IsNaN(coefficient))
+            {
+                residual = -1;
+                return;
+            }
+            z = r.Add(z, coefficient);
             residual = Math.Sqrt(dotproduct_rr) / norm_b;
         }
     }

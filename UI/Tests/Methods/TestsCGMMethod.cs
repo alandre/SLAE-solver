8286c493086d13ec61c352ac6b0a9fe7bde7457c
AndrewUsoltsev <usol1996@gmail.com>
2018-04-03 23:51:10 +0700

    добавление к прошлому коммиту, вывод результата

--- UI/Tests/Methods/TestsCGMMethod.cs
+++ UI/Tests/Methods/TestsCGMMethod.cs
@@ -43,6 +43,10 @@ namespace Methods
             IVector b = denseMatrix.Multiply(resultActual);
 
             var result = loggingSolver.Solve(denseMatrix, x0, b);
+
+            foreach (var elem in result)
+                _testOutputHelper.WriteLine(elem.ToString());
+
             for (int i = 0; i < resultActual.Size; i++)
                 Assert.Equal(result[i], resultActual[i], 8);
 
